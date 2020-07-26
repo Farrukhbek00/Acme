4eb1881f5b8f591c2a96d3608dc32fbf62708644
Farrukhbek <farruhzokirov00@gmail.com>
2020-07-26 21:47:51 +0500

    all static words converted into non-static

--- acme/acmeapp/models.py
+++ acme/acmeapp/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class Home(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
     background_image = models.ImageField(upload_to='showcase')
 
     box_title = models.CharField(max_length=50)
     box_description = models.CharField(max_length=100)
     box_image = models.ImageField(upload_to='box')
 
-    nav = models.CharField(max_length=50)
+    footer = models.CharField(max_length=50)
 
 class About(models.Model):
     about_us = models.CharField(max_length=50)
@@ -21,6 +21,8 @@ class About(models.Model):
     our_job = models.CharField(max_length=50)
     job_info = models.CharField(max_length=500)
 
+    footer = models.CharField(max_length=50, default='Acme Web Design, Copyright &copy; 2017')
+
 class Service(models.Model):
     services = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
@@ -28,3 +30,5 @@ class Service(models.Model):
     price = models.CharField(max_length=50)
 
     quote_title = models.CharField(max_length=50)
+
+    footer = models.CharField(max_length=50, default='Acme Web Design, Copyright &copy; 2017')
--- acme/acmeapp/views.py
+++ acme/acmeapp/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Home, About, Service
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {})
+    index = get_object_or_404(Home, pk=1)
+    index2 = get_object_or_404(Home, pk=2)
+    index3 = get_object_or_404(Home, pk=3)
+    return render(request, 'index.html', {
+        'index': index,
+        'index2': index2,
+        'index3': index3,
+    })
 
 def about(request):
-    return render(request, 'about.html', {})
+    about = get_object_or_404(About, pk=1)
+    return render(request, 'about.html', {
+        'about': about,
+    })
 
 def services(request):
-    return render(request, 'services.html', {})
+    service = get_object_or_404(Service, pk=1)
+    service2 = get_object_or_404(Service, pk=2)
+    service3 = get_object_or_404(Service, pk=3)
+    return render(request, 'services.html', {
+        'service': service,
+        'service2': service2,
+        'service3': service3,
+    })
